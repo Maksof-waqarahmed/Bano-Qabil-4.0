6ee65b56dee3eadcb7752c2600031611a1cc3802
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-02-15 21:57:56 -0800

    done

---
+++ WebWrap (Girls)/app.js
@@ -0,0 +1,16 @@
+
+//Primitive Data type
+//String n hkhikh 
+//Number 12 12.23 -234
+// bool true / false
+// undefined 
+// null 
+// let a = null;
+//bigint 
+//symbol
+console.log(a);
+
+
+
+// Non primitive Dara type
+
