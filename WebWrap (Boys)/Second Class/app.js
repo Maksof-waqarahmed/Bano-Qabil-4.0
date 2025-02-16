027152178fe3c279c9e961d7c4c20bfcff5cdb17
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-02-16 06:58:22 -0800

    done

---
+++ WebWrap (Boys)/Second Class/app.js
@@ -0,0 +1,35 @@
+// let userNumber = +prompt("Please Enter your age:", 23);
+// userAge = Number(userAge); //hum is trh sy be number me conevrt kr skty h
+
+// if(userNumber >= 90){
+//     alert("A+");
+// }else if(userNumber >= 80){
+//     alert("A");
+// }else if(userNumber >= 70){
+//     alert("B");
+// }else{
+//     alert("Fail");
+// }
+
+// if(1){ 
+//     alert("Condition True");
+// }else{
+//     alert("False");
+// }
+// 1: true
+// 0: false
+
+
+// let daysNumber = 2;
+// switch(daysNumber){
+//     case 1:
+//         alert("Monday");
+//         break;
+//     case 2: 
+//         alert("Tuesday");
+//         break;
+//     default: 
+//         alert("Invalid Input");
+//         break;
+// }
+
