80b5fab5d182a02bfb07db508831e44797c92d3e
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-02-16 03:54:38 -0800

    done

---
+++ WebWrap (Girls)/Second Class/app.js
@@ -0,0 +1,49 @@
+// alert("Hello");
+// document.write("Bano Qabil");
+// console.log("Waqar");
+//575757
+/*
+jjjbjgjgvjgvjv
+*/
+//string
+//number 
+// boolean
+//undefined
+//null
+//big Int
+//symbol
+// var a;
+// a = "Number";
+// let b = 12;
+// b = 15;
+// const c = 12;
+// let d;
+// console.log(d);
+// let nameN = null;
+// console.log(nameN);
+// const big = 788463947349379573495734059754079;
+// document.write(big);
+
+var a = "Waqar";
+console.log(a + " Is a teacher");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
