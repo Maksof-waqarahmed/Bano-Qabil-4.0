ea715a6cf1de04d05d430a7af38f18909657d2da
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-05-18 18:50:05 +0500

    done

---
+++ WebWizard (Boys)/JavaScript/Day 1/app.js
@@ -0,0 +1,55 @@
+// var   → function-scoped, can be redeclared ✅ (not recommended)  
+
+// let   → block-scoped, can be reassigned ✅, can't be redeclared ❌ 
+
+// const → block-scoped, can't be reassigned ❌, can't be redeclared ❌
+
+// var age = 32;
+// var age = 34;
+
+// let age = 9797;
+// let age = 45;
+
+// let a;
+// a = 43;
+
+// const age = 3.1452;
+// const age = 34;
+// console.log(age);
+
+let userName = "Waqar";
+console.log(userName);
+
+userName = "Ahmed";
+userName = "Rana";
+console.log(userName);
+
+// Data Types: 
+// 1) Primitive
+// 1. String "Hello World"
+// 2. Number 11, 12.3, -343, 980,
+// 3. Boolean true/false
+// 4. BigInt
+// 5. undefined  
+// 6. null
+// 7. symbol("id")
+
+// 2) Non Premitive (reference)
+// 1) Function
+// 2) Object
+// 3) Array
+
+let bool = false;
+bool = "hghg";
+bool = 12434;
+
+let big = 23891364182461289461209461094n;
+let x = null;
+console.log(x);
+
+
+
+
+
+
+
---
+++ webWizard (Girls)/JavaScript/Day 2/app.js
@@ -0,0 +1,61 @@
+
+// document.write("Hello World!");
+// console.log('Hello World!');
+// alert("This website is not for teenagers!");
+
+// let varibale name = value;
+// = (Assignment Operator) 
+// const 
+// var (Specifier) 
+
+// let a = "Bano Qabil";
+
+// console.log(a);
+// let b;
+// b = "Waqar";
+// variable Declaration
+// Variable Initialization
+
+//vary //able
+
+
+
+
+let abc = "Waqar";
+console.log(abc);
+
+abc = "Ahmed";
+console.log(abc);
+
+let pie = 5446;
+console.log(pie);
+
+// var   → function-scoped, can be redeclared ✅ (not recommended)  
+
+// let   → block-scoped, can be reassigned ✅, can't be redeclared ❌ 
+
+
+
+// const → block-scoped, can't be reassigned ❌, can't be redeclared ❌
+
+
+
+
+
+
+
+let user  = "Waqar";
+console.log(user)
+
+// Can’t Start with number and special character except ( _ and $)
+// Cannot Have Spaces Between Variable Names: 
+// Use Camel case writing style or underscore ya snake case for spacing 
+// Cannot Use Reserved Keywords
+// Case-Sensitive
+
+let bool = null;
+console.log(bool);
+
+// data types: 
+// premitive
+// non premitive (refrecnce)
