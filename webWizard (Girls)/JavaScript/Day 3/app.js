883a362e32d7bac53bdaa7f0afa7803e2e5435b3
Waqar Ahmed <160152452+Maksof-waqarahmed@users.noreply.github.com>
2025-05-28 17:45:13 +0500

    done

---
+++ webWizard (Girls)/JavaScript/Day 3/app.js
@@ -0,0 +1,36 @@
+
+
+const a = "Hello, World!";
+// const b = "Computer Teacher";
+
+// const b = a.length;
+// console.log(a.slice(7, -1));
+// console.log(a.slice(0, 5));
+// console.log(a.substring(0, 13))
+// console.log(a.replace("World", "JavaScript"));
+// console.log(a.replaceAll("abc", "Waqar"));
+// console.log(a.toUpperCase());
+// console.log(a.toLocaleLowerCase());
+// console.log(a.concat(" ", b, " Bano Qabil"));
+// console.log(a.charAt(6));
+// console.log(a.indexOf("a"));
+// console.log(a.lastIndexOf("a"));
+// console.log(a.trim());
+// console.log(a.trimStart());
+// console.log(a.trimEnd());
+// console.log(a.repeat(5));
+// console.log(a.includes("Waqar"));
+// console.log(a.startsWith("Waqar"));
+// console.log(a.endsWith("Ahmed"));
+// console.log(a.split(", "));
+
+// console.log(a)
+
+
+
+
+
+
+
+
+ 
